8de2a50b4c279efd0414dfbbc8de072201ada30a
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2024-10-31 09:58:36 +0100

    added a class for cors origin

---
+++ src/main/java/tn/esprit/tpfoyer/control/WebConfig.java
@@ -0,0 +1,19 @@
+package tn.esprit.tpfoyer.control;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://192.168.1.24","*") // Allow your Angular app's origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
