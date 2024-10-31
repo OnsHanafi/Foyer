31b3d4476e8c884bb0ffd9721b50b66516f455d8
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2024-10-31 10:55:06 +0100

    cors origin

--- src/main/java/tn/esprit/tpfoyer/control/WebConfig.java
+++ src/main/java/tn/esprit/tpfoyer/control/WebConfig.java
@@ -1,17 +1,9 @@
 package tn.esprit.tpfoyer.control;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.Collections;
-
 
 @Configuration
 @EnableWebMvc
@@ -19,30 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
-                .addMapping("/**")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowedOrigins("*")
-                .allowCredentials(true);
+                .addMapping("/**")  // Allow all endpoints
+                .allowedMethods("*")  // Allow all HTTP methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowedOrigins("*")  // Allow all origins
+                .allowCredentials(true);  // Allow credentials if needed
     }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type",
-                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-        config.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept","Authorization",
-                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-        config.setAllowCredentials(true);
-
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-
-
 }
