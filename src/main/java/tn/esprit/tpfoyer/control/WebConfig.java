e8d40f743f6c5a1d0ee125a067216514c869344d
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2024-10-31 12:48:08 +0100

    added back webconfig

--- src/main/java/tn/esprit/tpfoyer/control/WebConfig.java
+++ src/main/java/tn/esprit/tpfoyer/control/WebConfig.java
@@ -1,29 +1,29 @@
-//package tn.esprit.tpfoyer.control;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig {
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry
-//                        .addMapping("/tpfoyer/foyer/**")  // Allow all endpoints
-//                        .allowedOrigins("http://192.168.1.24:80","http://192.168.1.24")  // Allow all origins
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow all HTTP methods
-//                        .allowedHeaders("Content-Type")   // Allow all headers
-//                        .allowCredentials(true);  // Allow credentials if needed
-//
-//            }
-//        };
-//
-//    }
-//}
+package tn.esprit.tpfoyer.control;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/tpfoyer/foyer/**")  // Allow all endpoints
+                        .allowedOrigins("*")  // Allow all origins
+                        .allowedMethods("*")  // Allow all HTTP methods
+                        .allowedHeaders("*")   // Allow all headers
+                        .allowCredentials(false);  // Allow credentials if needed
+
+            }
+        };
+
+    }
+}
