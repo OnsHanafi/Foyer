b108e0fe81b684c59939606a4afa6cc2dda620b8
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2024-10-31 11:16:35 +0100

    cors origin

--- src/main/java/tn/esprit/tpfoyer/control/FoyerRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/FoyerRestController.java
@@ -10,17 +10,20 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/foyer")
+@CrossOrigin(origins = "*")
 public class FoyerRestController {
 
     IFoyerService foyerService;
 
     // http://localhost:8089/tpfoyer/foyer/retrieve-all-foyers
+    @CrossOrigin(origins = "*")
     @GetMapping("/retrieve-all-foyers")
     public List<Foyer> getFoyers() {
         List<Foyer> listFoyers = foyerService.retrieveAllFoyers();
         return listFoyers;
     }
     // http://localhost:8089/tpfoyer/foyer/retrieve-foyer/8
+    @CrossOrigin(origins = "*")
     @GetMapping("/retrieve-foyer/{foyer-id}")
     public Foyer retrieveFoyer(@PathVariable("foyer-id") Long fId) {
         Foyer foyer = foyerService.retrieveFoyer(fId);
@@ -28,6 +31,7 @@ public class FoyerRestController {
     }
 
     // http://localhost:8089/tpfoyer/foyer/add-foyer
+    @CrossOrigin(origins = "*")
     @PostMapping("/add-foyer")
     public Foyer addFoyer(@RequestBody Foyer f) {
         Foyer foyer = foyerService.addFoyer(f);
@@ -35,12 +39,14 @@ public class FoyerRestController {
     }
 
     // http://localhost:8089/tpfoyer/foyer/remove-foyer/{foyer-id}
+    @CrossOrigin(origins = "*")
     @DeleteMapping("/remove-foyer/{foyer-id}")
     public void removeFoyer(@PathVariable("foyer-id") Long fId) {
         foyerService.removeFoyer(fId);
     }
 
     // http://localhost:8089/tpfoyer/foyer/modify-foyer
+    @CrossOrigin(origins = "*")
     @PutMapping("/modify-foyer")
     public Foyer modifyFoyer(@RequestBody Foyer f) {
         Foyer foyer = foyerService.modifyFoyer(f);
