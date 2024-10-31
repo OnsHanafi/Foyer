50b088c6509d8dadab15b87af2194a3fd412912b
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2024-10-31 12:53:10 +0100

    new cors

--- src/main/java/tn/esprit/tpfoyer/control/CorsConfig.java
+++ src/main/java/tn/esprit/tpfoyer/control/CorsConfig.java
@@ -3,27 +3,21 @@ package tn.esprit.tpfoyer.control;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
-public class WebConfig {
-
+public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry
-                        .addMapping("/tpfoyer/foyer/**")  // Allow all endpoints
-                        .allowedOrigins("*")  // Allow all origins
-                        .allowedMethods("*")  // Allow all HTTP methods
-                        .allowedHeaders("*")   // Allow all headers
-                        .allowCredentials(false);  // Allow credentials if needed
-
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
-
     }
+
+
 }
